503353e9a4f0eadbd7b095fe1098e67b43b5bbdd
ShowyCone <alexrafael2003@gmail.com>
2024-03-15 03:05:14 -0400

    Bresenham with new graphics.js

--- 3-Bresenham/script.js
+++ 3-Bresenham/script.js
@@ -1,63 +1,41 @@
-function bresenhamLine(x0, y0, x1, y1) {
-    let dx = Math.abs(x1 - x0);
-    let dy = Math.abs(y1 - y0);
-    let sx = (x0 < x1) ? 1 : -1;
-    let sy = (y0 < y1) ? 1 : -1;
-    let err = dx - dy;
+import Drawing from '../graphics.js'
 
-    while(true) {
-        setPixel(x0, y0);
-
-        if ((x0 === x1) && (y0 === y1)) break;
-        let e2 = 2 * err;
-        if (e2 > -dy) { err -= dy; x0 += sx; }
-        if (e2 < dx) { err += dx; y0 += sy; }
-    }
+const drawBtn = document.getElementById('drawBtn')
+const clearBtn = document.getElementById('clearBtn')
+const randomBtn = document.getElementById('randomBtn')
+const canvas = document.getElementById('myCanvas')
+const canvasHeight = canvas.height
+const canvasWidth = canvas.width
+const ctx = canvas.getContext('2d')
+const coords0 = {
+  x: document.getElementById('x0'),
+  y: document.getElementById('y0'),
+}
+const coords1 = {
+  x: document.getElementById('x1'),
+  y: document.getElementById('y1'),
 }
 
-function setPixel(x, y) {
-    let canvas = document.getElementById('myCanvas');
-    let ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'black';
-    ctx.fillRect(x, y, 1, 1);
-}
+const drawing = new Drawing(ctx)
 
-function dibujar() {
-    let x0 = parseInt(document.getElementById('x0').value);
-    let y0 = parseInt(document.getElementById('y0').value);
-    let x1 = parseInt(document.getElementById('x1').value);
-    let y1 = parseInt(document.getElementById('y1').value);
-    bresenhamLine(x0, y0, x1, y1);
-}
+drawBtn.addEventListener('click', () => {
+  const x0 = parseInt(coords0.x.value)
+  const y0 = parseInt(coords0.y.value)
+  const x1 = parseInt(coords1.x.value)
+  const y1 = parseInt(coords1.y.value)
 
-function limpiar() {
-    let canvas = document.getElementById('myCanvas');
-    let ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
+  drawing.drawBresenhamLine(x0, y0, x1, y1)
+})
 
-function aleatorio() {
-    let x0 = Math.floor(Math.random() * 500);
-    let y0 = Math.floor(Math.random() * 500);
-    let x1 = Math.floor(Math.random() * 500);
-    let y1 = Math.floor(Math.random() * 500);
-    document.getElementById('x0').value = x0;
-    document.getElementById('y0').value = y0;
-    document.getElementById('x1').value = x1;
-    document.getElementById('y1').value = y1;
-    dibujar();
-}
+clearBtn.addEventListener('click', () => {
+  drawing.clearCanvas(canvasWidth, canvasHeight)
+})
 
-function validation() {
-    let x0 = document.getElementById('x0').value;
-    let y0 = document.getElementById('y0').value;
-    let x1 = document.getElementById('x1').value;
-    let y1 = document.getElementById('y1').value;
-    let btnDibujar = document.getElementById('btnDibujar');
-
-    btnDibujar.disabled = !(x0 && y0 && x1 && y1 &&
-                            x0 >= 0 && x0 <= 500 &&
-                            y0 >= 0 && y0 <= 500 &&
-                            x1 >= 0 && x1 <= 500 &&
-                            y1 >= 0 && y1 <= 500);
-}
+randomBtn.addEventListener('click', () => {
+  drawing.drawBresenhamLine(
+    Math.floor(Math.random() * 501),
+    Math.floor(Math.random() * 501),
+    Math.floor(Math.random() * 501),
+    Math.floor(Math.random() * 501)
+  )
+})
