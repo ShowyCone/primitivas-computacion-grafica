3f1ca36665d224dc9403b5c4627d876352e48e70
ShowyCone <alexrafael2003@gmail.com>
2024-03-15 02:52:46 -0400

    graphics.js functional to oop and remake ellipse script

--- 1-Elipse/script.js
+++ 1-Elipse/script.js
@@ -1,123 +1,41 @@
-const canvas = document.getElementById('canvas')
+import Drawing from '../graphics.js'
+
+const drawBtn = document.getElementById('drawBtn')
+const clearBtn = document.getElementById('clearBtn')
+const randomBtn = document.getElementById('randomBtn')
+const canvas = document.getElementById('myCanvas')
+const canvasHeight = canvas.height
+const canvasWidth = canvas.width
 const ctx = canvas.getContext('2d')
-
-canvas.width = 400
-canvas.height = 400
-
-const defaultData = {
-  radioX: 120,
-  radioY: 50,
+const coords = {
+  x: document.getElementById('xCoord'),
+  y: document.getElementById('yCoord'),
+}
+const radius = {
+  x: document.getElementById('xRadius'),
+  y: document.getElementById('yRadius'),
 }
 
-function clearCanvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-}
+const drawing = new Drawing(ctx)
 
-document.getElementById('xRange').addEventListener('input', (event) => {
-  drawEllipse(undefined, undefined, event.target.value)
+drawBtn.addEventListener('click', () => {
+  drawing.drawEllipse(
+    coords.x.value,
+    coords.y.value,
+    radius.x.value,
+    radius.y.value
+  )
 })
 
-document.getElementById('yRange').addEventListener('input', (event) => {
-  drawEllipse(undefined, undefined, undefined, event.target.value)
+clearBtn.addEventListener('click', () => {
+  drawing.clearCanvas(canvasWidth, canvasHeight)
 })
 
-drawEllipse(undefined, undefined, undefined, undefined)
-
-function drawEllipse(
-  centerX = canvas.width / 2,
-  centerY = canvas.height / 2,
-  width = defaultData.radioX,
-  height = defaultData.radioY
-) {
-  clearCanvas()
-  defaultData.radioX = width
-  defaultData.radioY = height
-  ctx.beginPath()
-
-  ctx.moveTo(centerX, centerY - height / 2)
-
-  ctx.bezierCurveTo(
-    centerX + width / 2,
-    centerY - height / 2,
-    centerX + width / 2,
-    centerY + height / 2,
-    centerX,
-    centerY + height / 2
+randomBtn.addEventListener('click', () => {
+  drawing.drawEllipse(
+    Math.floor(Math.random() * 501),
+    Math.floor(Math.random() * 501),
+    Math.floor(Math.random() * 251),
+    Math.floor(Math.random() * 251)
   )
-
-  ctx.bezierCurveTo(
-    centerX - width / 2,
-    centerY + height / 2,
-    centerX - width / 2,
-    centerY - height / 2,
-    centerX,
-    centerY - height / 2
-  )
-
-  ctx.fillStyle = 'blue'
-  ctx.fill()
-  ctx.strokeStyle = 'black'
-  ctx.stroke()
-
-  const ellipseInfo = getEllipseInfo(width, height)
-  const bezierInfo = getBezierInfo(centerX, centerY, width, height)
-  const bezierFormula = getBezierFormula(bezierInfo)
-
-  setAllInfo(ellipseInfo, bezierInfo, bezierFormula)
-}
-
-function getEllipseInfo(radioX, radioY) {
-  return {
-    radioX,
-    radioY,
-    area: (Math.PI * radioX * radioY).toFixed(4),
-    circumference: (
-      2 *
-      Math.PI *
-      Math.sqrt((radioX ** 2 + radioY ** 2) / 2)
-    ).toFixed(4),
-  }
-}
-
-function getBezierInfo(centerX, centerY, width, height) {
-  const controlPointX = centerX + width / 2
-  const controlPointY = centerY + height / 2
-
-  return {
-    controlPointX,
-    controlPointY,
-    startPoint: { x: centerX, y: centerY - height / 2 },
-    endPoint: { x: centerX, y: centerY + height / 2 },
-  }
-}
-
-function getBezierFormula(bezierInfo) {
-  const { startPoint, controlPointX, controlPointY, endPoint } = bezierInfo
-
-  return `B(t) = (1 - t)^3 * (${startPoint.x}, ${startPoint.y}) + 3t(1 - t)^2 * (${controlPointX}, ${controlPointY}) + 3t^2(1 - t) * (${controlPointX}, ${controlPointY}) + t^3 * (${endPoint.x}, ${endPoint.y})`
-}
-
-function setAllInfo(ellipseInfo, bezierInfo, bezierData) {
-  const allEllipseToSet = document.querySelectorAll('.ellipse-info')
-  const allBezierToSet = document.querySelectorAll('.bezier-info')
-  const bezierToData = document.querySelector('.bezier-data')
-
-  let ellipseInfoInArr = [
-    ellipseInfo.area,
-    ellipseInfo.circumference,
-    ellipseInfo.radioX,
-    ellipseInfo.radioY,
-  ]
-
-  let bezierInfoInArr = [bezierInfo.controlPointX, bezierInfo.controlPointY]
-
-  allEllipseToSet.forEach((ellipseToSet, index) => {
-    ellipseToSet.innerHTML = ellipseInfoInArr[index]
-  })
-
-  allBezierToSet.forEach((bezierToSet, index) => {
-    bezierToSet.innerHTML = bezierInfoInArr[index]
-  })
-
-  bezierToData.innerHTML = bezierData
-}
+})
--- graphics.js
+++ graphics.js
@@ -1,178 +1,212 @@
-/**
- * Dibuja una línea utilizando el algoritmo de Bresenham en un contexto dado.
- * @param {CanvasRenderingContext2D} ctx - El contexto de renderizado del canvas.
- * @param {number} x0 - La coordenada x inicial.
- * @param {number} y0 - La coordenada y inicial.
- * @param {number} x1 - La coordenada x final.
- * @param {number} y1 - La coordenada y final.
- */
-function drawBresenhamLine(ctx, x0, y0, x1, y1) {
-  let dx = Math.abs(x1 - x0)
-  let dy = Math.abs(y1 - y0)
-  let sx = x0 < x1 ? 1 : -1
-  let sy = y0 < y1 ? 1 : -1
-  let err = dx - dy
+export default class Drawing {
+  /**
+   * Constructor de la clase Drawing.
+   * @param {CanvasRenderingContext2D} ctx - El contexto de renderizado del canvas.
+   */
+  constructor(ctx) {
+    this.ctx = ctx
+    this.lineColor = 'black'
+    this.lineWidth = 1
+    this.lineStyle = 'solid'
+  }
 
-  while (true) {
-    ctx.fillStyle = "black"
-    ctx.fillRect(x0, y0, 1, 1)
+  /**
+   * Establece el color de línea.
+   * @param {string} color - El color de línea a establecer.
+   */
+  setLineColor(color) {
+    this.lineColor = color
+  }
 
-    if (x0 === x1 && y0 === y1) break
-    let e2 = 2 * err
-    if (e2 > -dy) {
-      err -= dy
-      x0 += sx
+  /**
+   * Establece el ancho de línea.
+   * @param {number} width - El ancho de línea a establecer.
+   */
+  setLineWidth(width) {
+    this.lineWidth = width
+  }
+
+  /**
+   * Establece el estilo de línea.
+   * @param {string} style - El estilo de línea a establecer.
+   */
+  setLineStyle(style) {
+    this.lineStyle = style
+  }
+
+  /**
+   * Limpia el contenido del canvas.
+   * @param {number} canvasWidth - Ancho del canvas.
+   * @param {number} canvasHeight - Alto del canvas.
+   */
+  clearCanvas(canvasWidth, canvasHeight) {
+    this.ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+  }
+
+  /**
+   * Dibuja una elipse en el contexto del canvas.
+   * @param {number} x - La coordenada x del centro de la elipse.
+   * @param {number} y - La coordenada y del centro de la elipse.
+   * @param {number} radiusX - El radio x de la elipse.
+   * @param {number} radiusY - El radio y de la elipse.
+   */
+  drawEllipse(x, y, radiusX, radiusY) {
+    this.ctx.beginPath()
+    this.ctx.ellipse(x, y, radiusX, radiusY, 0, 0, Math.PI * 2)
+    this.ctx.stroke()
+  }
+
+  /**
+   * Dibuja un punto en el contexto del canvas.
+   * @param {number} x - La coordenada x del punto.
+   * @param {number} y - La coordenada y del punto.
+   */
+  drawPoint(x, y) {
+    this.ctx.fillRect(x, y, 1, 1)
+  }
+
+  /**
+   * Dibuja un círculo en el contexto del canvas.
+   * @param {number} x - La coordenada x del centro del círculo.
+   * @param {number} y - La coordenada y del centro del círculo.
+   * @param {number} radius - El radio del círculo.
+   */
+  drawCircle(x, y, radius) {
+    this.ctx.beginPath()
+    this.ctx.arc(x, y, radius, 0, Math.PI * 2)
+    this.ctx.stroke()
+  }
+
+  /**
+   * Dibuja una línea utilizando el algoritmo de Bresenham en el contexto del canvas.
+   * @param {number} x0 - La coordenada x inicial de la línea.
+   * @param {number} y0 - La coordenada y inicial de la línea.
+   * @param {number} x1 - La coordenada x final de la línea.
+   * @param {number} y1 - La coordenada y final de la línea.
+   */
+  drawBresenhamLine(x0, y0, x1, y1) {
+    let dx = Math.abs(x1 - x0)
+    let dy = Math.abs(y1 - y0)
+    let sx = x0 < x1 ? 1 : -1
+    let sy = y0 < y1 ? 1 : -1
+    let err = dx - dy
+
+    while (true) {
+      this.ctx.fillStyle = this.lineColor
+      this.ctx.fillRect(x0, y0, 1, 1)
+
+      if (x0 === x1 && y0 === y1) break
+      let e2 = 2 * err
+      if (e2 > -dy) {
+        err -= dy
+        x0 += sx
+      }
+      if (e2 < dx) {
+        err += dx
+        y0 += sy
+      }
     }
-    if (e2 < dx) {
-      err += dx
-      y0 += sy
+  }
+
+  /**
+   * Dibuja una línea utilizando el algoritmo DDA en el contexto del canvas.
+   * @param {number} x0 - La coordenada x inicial de la línea.
+   * @param {number} y0 - La coordenada y inicial de la línea.
+   * @param {number} x1 - La coordenada x final de la línea.
+   * @param {number} y1 - La coordenada y final de la línea.
+   */
+  drawDDALine(x0, y0, x1, y1) {
+    let dx = x1 - x0
+    let dy = y1 - y0
+    let steps = Math.abs(dx) > Math.abs(dy) ? Math.abs(dx) : Math.abs(dy)
+    let incrementoX = dx / steps
+    let incrementoY = dy / steps
+    let x = x0
+    let y = y0
+    this.ctx.fillStyle = this.lineColor
+    for (let i = 0; i <= steps; i++) {
+      this.ctx.fillRect(Math.round(x), Math.round(y), 1, 1)
+      x += incrementoX
+      y += incrementoY
     }
   }
-}
 
-/**
- * Dibuja un punto en un contexto dado.
- * @param {CanvasRenderingContext2D} ctx - El contexto de renderizado del canvas.
- * @param {number} x - La coordenada x del punto.
- * @param {number} y - La coordenada y del punto.
- */
-function drawPoint(ctx, x, y) {
-  ctx.fillStyle = "black"
-  ctx.beginPath()
-  ctx.arc(x, y, 1, 0, Math.PI * 2, true)
-  ctx.fill()
-}
-
-/**
- * Dibuja una línea utilizando el algoritmo DDA en un contexto dado.
- * @param {CanvasRenderingContext2D} ctx - El contexto de renderizado del canvas.
- * @param {number} x0 - La coordenada x inicial.
- * @param {number} y0 - La coordenada y inicial.
- * @param {number} x1 - La coordenada x final.
- * @param {number} y1 - La coordenada y final.
- */
-function drawDDALine(ctx, x0, y0, x1, y1) {
-  let dx = x1 - x0
-  let dy = y1 - y0
-  let steps = Math.abs(dx) > Math.abs(dy) ? Math.abs(dx) : Math.abs(dy)
-  let incrementoX = dx / steps
-  let incrementoY = dy / steps
-  let x = x0
-  let y = y0
-  ctx.fillStyle = "black"
-  for (let i = 0; i <= steps; i++) {
-    ctx.fillRect(Math.round(x), Math.round(y), 1, 1)
-    x += incrementoX
-    y += incrementoY
-  }
-}
-
-/**
- * Rellena un polígono utilizando el algoritmo de relleno de líneas de exploración.
- * @param {CanvasRenderingContext2D} ctx - El contexto de renderizado del canvas.
- * @param {Array} vertices - Los vértices del polígono en el formato [[x1, y1], [x2, y2], ..., [xn, yn]].
- */
-function fillPolygon(ctx, vertices) {
-  let minY = vertices[0][1]
-  let maxY = vertices[0][1]
-  for (let i = 1; i < vertices.length; i++) {
-    if (vertices[i][1] < minY) minY = vertices[i][1]
-    if (vertices[i][1] > maxY) maxY = vertices[i][1]
+  /**
+   * Dibuja un polígono regular en el contexto del canvas.
+   * @param {number} centerX - La coordenada x del centro del polígono.
+   * @param {number} centerY - La coordenada y del centro del polígono.
+   * @param {number} sides - El número de lados del polígono.
+   * @param {number} radius - El radio del polígono.
+   */
+  drawRegularPolygon(centerX, centerY, sides, radius) {
+    this.ctx.beginPath()
+    const angle = (Math.PI * 2) / sides
+    for (let i = 0; i < sides; i++) {
+      const x = centerX + radius * Math.cos(i * angle)
+      const y = centerY + radius * Math.sin(i * angle)
+      if (i === 0) {
+        this.ctx.moveTo(x, y)
+      } else {
+        this.ctx.lineTo(x, y)
+      }
+    }
+    this.ctx.closePath()
+    this.ctx.stroke()
   }
 
-  for (let y = minY; y <= maxY; y++) {
-    let points = []
+  /**
+   * Rellena una forma cerrada en el contexto del canvas.
+   * @param {number} x - La coordenada x de la esquina superior izquierda del área a rellenar.
+   * @param {number} y - La coordenada y de la esquina superior izquierda del área a rellenar.
+   * @param {number} width - El ancho del área a rellenar.
+   * @param {number} height - La altura del área a rellenar.
+   */
+  fillShape(x, y, width, height) {
+    const imageData = this.ctx.getImageData(x, y, width, height)
+    const data = imageData.data
 
-    for (let i = 0; i < vertices.length; i++) {
-      let j = (i + 1) % vertices.length
-      let x1 = vertices[i][0],
-        y1 = vertices[i][1]
-      let x2 = vertices[j][0],
-        y2 = vertices[j][1]
-      if ((y1 < y && y2 >= y) || (y2 < y && y1 >= y)) {
-        let x = x1 + ((y - y1) / (y2 - y1)) * (x2 - x1)
-        points.push(x)
+    const fillColor = this.ctx.fillStyle
+    const fillColorArray = this.hexToRgb(fillColor)
+
+    const isInside = (x, y) => {
+      const index = (y - imageData.y) * width * 4 + (x - imageData.x) * 4
+      return (
+        data[index] === fillColorArray[0] &&
+        data[index + 1] === fillColorArray[1] &&
+        data[index + 2] === fillColorArray[2]
+      )
+    }
+
+    const fillHorizontalLine = (x1, x2, y) => {
+      for (let x = x1; x <= x2; x++) {
+        const index = (y - imageData.y) * width * 4 + (x - imageData.x) * 4
+        data[index] = fillColorArray[0]
+        data[index + 1] = fillColorArray[1]
+        data[index + 2] = fillColorArray[2]
       }
     }
 
-    points.sort((a, b) => a - b)
-    for (let i = 0; i < points.length; i += 2) {
-      ctx.beginPath()
-      ctx.moveTo(points[i], y)
-      ctx.lineTo(points[i + 1], y)
-      ctx.stroke()
+    for (let y = 0; y < height; y++) {
+      let inside = false
+
+      for (let x = 0; x < width; x++) {
+        const index = (y * width + x) * 4
+
+        if (isInside(x, y)) {
+          inside = !inside
+        }
+
+        if (inside) {
+          let startX = x
+          while (inside && x < width) {
+            x++
+            inside = isInside(x, y)
+          }
+          fillHorizontalLine(startX, x - 1, y)
+        }
+      }
     }
+
+    this.ctx.putImageData(imageData, x, y)
   }
-}
-
-/**
- * Dibuja un polígono regular basado en el número de lados y el radio.
- * @param {CanvasRenderingContext2D} ctx - El contexto de renderizado del canvas.
- * @param {number} centerX - La coordenada x del centro del polígono.
- * @param {number} centerY - La coordenada y del centro del polígono.
- * @param {number} sides - El número de lados del polígono.
- * @param {number} radius - El radio del polígono.
- */
-function drawRegularPolygon(ctx, centerX, centerY, sides, radius) {
-  ctx.beginPath()
-  let angle = (Math.PI * 2) / sides
-  for (let i = 0; i <= sides; i++) {
-    let x = centerX + radius * Math.cos(i * angle)
-    let y = centerY + radius * Math.sin(i * angle)
-    if (i === 0) {
-      ctx.moveTo(x, y)
-    } else {
-      ctx.lineTo(x, y)
-    }
-  }
-  ctx.closePath()
-  ctx.stroke()
-}
-
-/**
- * Dibuja un círculo con un radio y centro dados.
- * @param {CanvasRenderingContext2D} ctx - El contexto de renderizado del canvas.
- * @param {number} centerX - La coordenada x del centro del círculo.
- * @param {number} centerY - La coordenada y del centro del círculo.
- * @param {number} radius - El radio del círculo.
- */
-function drawCircle(ctx, centerX, centerY, radius) {
-  ctx.beginPath()
-  ctx.arc(centerX, centerY, radius, 0, Math.PI * 2)
-  ctx.stroke()
-}
-
-/**
- * Dibuja un círculo con un radio y centro dados.
- * @param {CanvasRenderingContext2D} ctx - El contexto de renderizado del canvas.
- * @param {number} centerX - La coordenada x del centro del círculo.
- * @param {number} centerY - La coordenada y del centro del círculo.
- * @param {number} radiusX - El radio x de la elipse.
- * @param {number} radiusY - El radio y de la elipse.
- */
-function drawEllipse(ctx, centerX, centerY, radiusX, radiusY) {
-  ctx.beginPath()
-  ctx.ellipse(centerX, centerY, radiusX, radiusY, 0, 0, 2 * Math.PI)
-  ctx.stroke()
-}
-
-/**
- *
- * @param {CanvasRenderingContext2D} ctx - El contexto de renderizado del canvas.
- * @param {number} canvasWidth - Ancho del canvas
- * @param {number} canvasHeight - Alto del canvas
- */
-function clearCanvas(ctx, canvasWidth, canvasHeight) {
-  ctx.clearRect(0, 0, canvasWidth, canvasHeight)
-}
-
-module.exports = {
-  drawBresenhamLine,
-  drawPoint,
-  drawDDALine,
-  fillPolygon,
-  drawRegularPolygon,
-  drawCircle,
-  drawEllipse,
-  clearCanvas,
 }
